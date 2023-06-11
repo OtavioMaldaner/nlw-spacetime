27bba99254e5fa5d811fdad728e0f6e2c3651297
OtavioMaldaner <otaviomaldaner571@gmail.com>
2023-06-11 16:23:25 -0300

    Reorganização front-end e início de autenticação

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 import { z } from 'zod'
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+    return (
+        <div className="text-sm leading-relaxed text-gray-200">
+            Feito com 💜 no NLW da{' '}
+            <a
+                target="_blank"
+                className="underline transition-all hover:text-gray-200"
+                href="https://rocketseat.com.br"
+                rel="noreferrer"
+            >
+                Rocketseat
+            </a>
+        </div>
+    )
+}
